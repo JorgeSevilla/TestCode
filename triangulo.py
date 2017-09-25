edbdf569d2c70c25128e3abdcc46c8574924194a
Jorge Sevilla <solopunks803@gmail.com>
2017-09-25 20:37:31 -0300

    Tree
    Make a tree

---
+++ triangulo.py
@@ -0,0 +1,17 @@
+#n = 0
+# for i in range(1, 10):
+#     for j in range(i):
+#         print '*'
+#     print i
+tam2 = 5
+ancho = 1
+tam = 1
+ancho2 = 1
+for b in range(tam):
+    print '%s%s' % (' ' * (tam2 - 3), '*' * ancho2)
+    print '%s%s' % (' ' * (tam2 - 4), '*' * ancho2), '%s%s' % (' ' * (tam2 - 5), '*' * ancho2)
+    print '%s%s' % (' ' * (tam2 - 5), '*' * ancho2), '%s%s' % (' ' * (tam2 - 6), '*' * ancho2), \
+            '%s%s' % (' ' * (tam2 - 7), '*' * ancho2)
+
+for a in range(tam2):
+    print '%s%s' % (' '*(tam2 - 3), '*'*ancho)
